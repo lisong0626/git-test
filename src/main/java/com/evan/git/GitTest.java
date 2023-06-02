74d761428afd5fd99e2351c182303e0980a45212
Evan <lisong0626@icloud.com>
2023-06-02 18:11:16 +0800

    hot-fix commit-1

--- src/main/java/com/evan/git/GitTest.java
+++ src/main/java/com/evan/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello,Git");
         System.out.println("Hello,GitHub");
         System.out.println("Hello,Git2");
+        System.out.println("hello,gir1");
     }
 }
