023b72a27a158358059bb4f2efd6c457dd142c53
Evan <lisong0626@icloud.com>
2023-06-02 18:11:16 +0800

    master commit-1

--- src/main/java/com/evan/git/GitTest.java
+++ src/main/java/com/evan/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("Hello,Git");
         System.out.println("Hello,GitHub");
         System.out.println("Hello,Git2");
+        System.out.println("hello,gir1");
+        System.out.println("hot-fix test");
     }
 }
